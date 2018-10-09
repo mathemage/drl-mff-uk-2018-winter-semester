215401c8900310178f474a9027454fced250ad6f
Karel Ha <mathemage@gmail.com>
2018-10-09 18:52:59 +0200

    Add experiments for UCB and gradient
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- labs/01/run_multiarmed_bandits.sh
+++ labs/01/run_multiarmed_bandits.sh
@@ -22,3 +22,16 @@ ${PYTHON} ${SCRIPT} --initial 1 --alpha 0.15 --epsilon 0.0078125 # 1/128
 ${PYTHON} ${SCRIPT} --initial 1 --alpha 0.15 --epsilon 0.015625  # 1/64
 ${PYTHON} ${SCRIPT} --initial 1 --alpha 0.15 --epsilon 0.03125   # 1/32
 ${PYTHON} ${SCRIPT} --initial 1 --alpha 0.15 --epsilon 0.0625    # 1/16
+
+# 4) UCB
+${PYTHON} ${SCRIPT} --mode ucb --c 0.0078125 # 1/128
+${PYTHON} ${SCRIPT} --mode ucb --c 0.015625  # 1/64
+${PYTHON} ${SCRIPT} --mode ucb --c 0.03125   # 1/32
+${PYTHON} ${SCRIPT} --mode ucb --c 0.0625    # 1/16
+${PYTHON} ${SCRIPT} --mode ucb --c 0.125     # 1/8
+
+# 5) gradient
+${PYTHON} ${SCRIPT} --mode gradient --alpha 0.0625    # 1/16
+${PYTHON} ${SCRIPT} --mode gradient --alpha 0.125     # 1/8
+${PYTHON} ${SCRIPT} --mode gradient --alpha 0.25      # 1/4
+${PYTHON} ${SCRIPT} --mode gradient --alpha 0.5       # 1/2
