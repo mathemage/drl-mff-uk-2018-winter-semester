35f4c1ba1a03aea6ff93e2bc1de57b5f3834c097
Karel Ha <mathemage@gmail.com>
2018-10-16 15:56:16 +0200

    Create run script for `policy_iteration.sh`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- labs/01/run_multiarmed_bandits.sh
+++ labs/01/run_multiarmed_bandits.sh
@@ -28,11 +28,11 @@ ${PYTHON} ${SCRIPT} --initial 1 --alpha 0.15 --epsilon 0.03125   # 1/32
 ${PYTHON} ${SCRIPT} --initial 1 --alpha 0.15 --epsilon 0.0625    # 1/16
 
 echo "4) UCB:" >>${OUTPUT_FILE}
-${PYTHON} ${SCRIPT} --mode ucb --c 0.0078125 # 1/128
-${PYTHON} ${SCRIPT} --mode ucb --c 0.015625  # 1/64
-${PYTHON} ${SCRIPT} --mode ucb --c 0.03125   # 1/32
-${PYTHON} ${SCRIPT} --mode ucb --c 0.0625    # 1/16
-${PYTHON} ${SCRIPT} --mode ucb --c 0.125     # 1/8
+${PYTHON} ${SCRIPT} --mode ucb --c 0.25 # 1/4
+${PYTHON} ${SCRIPT} --mode ucb --c 0.5  # 1/2
+${PYTHON} ${SCRIPT} --mode ucb --c 1    # 1
+${PYTHON} ${SCRIPT} --mode ucb --c 2    # 2
+${PYTHON} ${SCRIPT} --mode ucb --c 4    # 4
 
 echo "5) gradient:" >>${OUTPUT_FILE}
 ${PYTHON} ${SCRIPT} --mode gradient --alpha 0.0625    # 1/16
