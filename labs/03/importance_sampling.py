176a76452be478eef169dcc7a21dbe378f26093b
ruda <jan.rudolf.steeldog@gmail.com>
2018-10-29 12:52:02 +0100

    Implementation of importance sampling - not working yet

--- labs/03/importance_sampling.py
+++ labs/03/importance_sampling.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
+#
+# All team solutions **must** list **all** members of the team.
+# The members must be listed using their ReCodEx ids anywhere
+# in the first comment block in the source file, i.e., in the first
+# consecutive range of lines beginning with `#`.
+#
+# You can find out ReCodEx id on URL when watching ReCodEx profile.
+# The id has the following format: 01234567-89ab-cdef-0123-456789abcdef.
+#
+# 090fa5b6-d3cf-11e8-a4be-00505601122b (Jan Rudolf)
+# 08a323e8-21f3-11e8-9de3-00505601122b (Karel Ha)
+#
+
 import numpy as np
 import gym
 
@@ -35,6 +48,35 @@ if __name__ == "__main__":
             state = next_state
 
         # TODO: Update V using weighted importance sampling.
+        G = 0
+        W = 1
+
+        # episode = episode[:-1]
+
+        for episode_sample in reversed(episode):
+            state, action, reward = episode_sample
+
+            G = G + reward
+
+            C[state] = C[state] + W
+
+            V[state] = V[state] + (W/C[state]) * (G - V[state])
+
+            # from https://github.com/openai/gym/blob/master/gym/envs/toy_text/frozen_lake.py
+            # LEFT = 0
+            # DOWN = 1
+            # RIGHT = 2
+            # UP = 3
+
+            if action == 1 or action == 2:
+                target_policy = 0.5
+            else:
+                target_policy = 0
+
+            W = W * (target_policy/0.25)
+            if W == 0:
+                break
+
 
     # Print the final value function V
     for row in V.reshape(4, 4):
