121cbbf37b56d0d3207de0c7b1e679df9bfe984b
Karel Ha <mathemage@gmail.com>
2018-11-01 16:46:08 +0100

    Create `importance_sampling_q.py`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ labs/03/importance_sampling_q.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+#
+# All team solutions **must** list **all** members of the team.
+# The members must be listed using their ReCodEx ids anywhere
+# in the first comment block in the source file, i.e., in the first
+# consecutive range of lines beginning with `#`.
+#
+# You can find out ReCodEx id on URL when watching ReCodEx profile.
+# The id has the following format: 01234567-89ab-cdef-0123-456789abcdef.
+#
+# 090fa5b6-d3cf-11e8-a4be-00505601122b (Jan Rudolf)
+# 08a323e8-21f3-11e8-9de3-00505601122b (Karel Ha)
+#
+
+import numpy as np
+import gym
+
+if __name__ == "__main__":
+	# Fix random seed
+	np.random.seed(42)
+
+	# Parse arguments
+	import argparse
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--episodes", default=1000, type=int, help="Training episodes.")
+	args = parser.parse_args()
+
+	# Create the environment
+	env = gym.make("FrozenLake-v0")
+	env.seed(42)
+	states = env.observation_space.n
+	actions = env.action_space.n
+
+	# Behaviour policy is uniformly random.
+	# Target policy uniformly chooses either action 1 or 2.
+	Q = np.zeros(states)    # TODO make Q 2D: (state, action)
+	C = np.zeros(states)    # TODO make C 2D: (state, action)
+
+	for _ in range(args.episodes):
+		state, done = env.reset(), False
+
+		# Generate episode
+		episode = []
+		while not done:
+			action = np.random.choice(actions)
+			next_state, reward, done, _ = env.step(action)
+			episode.append((state, action, reward))
+			state = next_state
+
+		# TODO: Update Q using weighted importance sampling.
+		G = 0
+		W = 1
+
+		# episode = episode[:-1]
+
+		for episode_sample in reversed(episode):
+			state, action, reward = episode_sample
+			G = G + reward
+			C[state] = C[state] + W  # TODO update this as 2D
+			Q[state] = Q[state] + (W / C[state]) * (G - Q[state])  # TODO update this as 2D
+
+			# from https://github.com/openai/gym/blob/master/gym/envs/toy_text/frozen_lake.py
+			# LEFT = 0
+			# DOWN = 1
+			# RIGHT = 2
+			# UP = 3
+			if action == 1 or action == 2:
+				target_policy = 0.5
+			else:
+				target_policy = 0
+			W = W * (target_policy/0.25)
+			if W == 0:
+				break
+
+	# Print the final value function V
+	# TODO compute V from Q (as sum); target policy = \pi, lect 2, slide 3
+	V = None
+	for row in V.reshape(4, 4):
+		print(" ".join(["{:5.2f}".format(x) for x in row]))
