583dc9a52243f75a2a5d9b26b330ef0f7cc378bf
ruda <jan.rudolf.steeldog@gmail.com>
2018-11-17 20:36:37 +0100

    Fixed funking oversighted usage argparse instead of amax! FUCK!

--- labs/04/q_learning_tiles.py
+++ labs/04/q_learning_tiles.py
@@ -14,9 +14,9 @@ if __name__ == "__main__":
     parser.add_argument("--episodes", default=10000, type=int, help="Training episodes.")
     parser.add_argument("--render_each", default=None, type=int, help="Render some episodes.")
 
-    parser.add_argument("--alpha", default=0.5, type=float, help="Learning rate.")
+    parser.add_argument("--alpha", default=0.1, type=float, help="Learning rate.")
     parser.add_argument("--alpha_final", default=0.01, type=float, help="Final learning rate.")
-    parser.add_argument("--epsilon", default=0.5, type=float, help="Exploration factor.")
+    parser.add_argument("--epsilon", default=0.2, type=float, help="Exploration factor.")
     parser.add_argument("--epsilon_final", default=0.001, type=float, help="Final exploration factor.")
     parser.add_argument("--gamma", default=1, type=float, help="Discounting factor.")
     parser.add_argument("--tiles", default=8, type=int, help="Number of tiles.") # default 8
@@ -30,11 +30,18 @@ if __name__ == "__main__":
     epsilon = args.epsilon
     alpha = args.alpha / args.tiles
 
+    #alpha_decay_step = (args.alpha - args.alpha_final) / args.episodes
+    #epsilon_decay_step = (args.epsilon - args.epsilon_final) / args.episodes
+
     flag_print = False
 
     evaluating = False
     while not evaluating:
+        #print('alpha - ', alpha, ', epsilon ', epsilon)
         # Perform a training episode
+        #alpha = args.alpha - env.episode * alpha_decay_step
+        #epsilon = args.epsilon - env.episode * epsilon_decay_step
+
         state, done = env.reset(evaluating), False
         while not done:
             if args.render_each and env.episode and env.episode % args.render_each == 0:
@@ -61,10 +68,10 @@ if __name__ == "__main__":
 
             # TODO: Update W values
             Q_s_a = W[state, action].sum()
-            Q_next_s_max = np.argmax(W[next_state].sum(axis=0))
-            # for i in state:
-            #     W[i, action] += alpha * (reward + args.gamma * Q_next_s_max - Q_s_a)
-            W[state, action] += alpha * (reward + args.gamma * Q_next_s_max - Q_s_a)
+            Q_next_s_max = np.amax(W[next_state].sum(axis=0))
+            for i in state:
+                W[i, action] += alpha * (reward + args.gamma * Q_next_s_max - Q_s_a)
+            # W[state, action] += alpha * (reward + args.gamma * Q_next_s_max - Q_s_a)
 
             state = copy.deepcopy(next_state)
             if done:
