c8f55c89f840ec8c878b8375dc35352f2931682a
ruda <jan.rudolf.steeldog@gmail.com>
2018-11-17 21:55:10 +0100

    Removes inefficient operations, TODOs, and set up parameters

--- labs/04/q_learning_tiles.py
+++ labs/04/q_learning_tiles.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
+#
+# All team solutions **must** list **all** members of the team.
+# The members must be listed using their ReCodEx ids anywhere
+# in the first comment block in the source file, i.e., in the first
+# consecutive range of lines beginning with `#`.
+#
+# You can find out ReCodEx id on URL when watching ReCodEx profile.
+# The id has the following format: 01234567-89ab-cdef-0123-456789abcdef.
+#
+# 090fa5b6-d3cf-11e8-a4be-00505601122b (Jan Rudolf)
+# 08a323e8-21f3-11e8-9de3-00505601122b (Karel Ha)
+#
 import numpy as np
-import copy
 
 import mountain_car_evaluator
 
@@ -11,12 +22,12 @@ if __name__ == "__main__":
     # Parse arguments
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("--episodes", default=10000, type=int, help="Training episodes.")
+    parser.add_argument("--episodes", default=7500, type=int, help="Training episodes.")
     parser.add_argument("--render_each", default=None, type=int, help="Render some episodes.")
 
-    parser.add_argument("--alpha", default=0.1, type=float, help="Learning rate.")
+    parser.add_argument("--alpha", default=0.2, type=float, help="Learning rate.")
     parser.add_argument("--alpha_final", default=0.01, type=float, help="Final learning rate.")
-    parser.add_argument("--epsilon", default=0.2, type=float, help="Exploration factor.")
+    parser.add_argument("--epsilon", default=0.1, type=float, help="Exploration factor.")
     parser.add_argument("--epsilon_final", default=0.001, type=float, help="Final exploration factor.")
     parser.add_argument("--gamma", default=1, type=float, help="Discounting factor.")
     parser.add_argument("--tiles", default=8, type=int, help="Number of tiles.") # default 8
@@ -30,54 +41,33 @@ if __name__ == "__main__":
     epsilon = args.epsilon
     alpha = args.alpha / args.tiles
 
-    #alpha_decay_step = (args.alpha - args.alpha_final) / args.episodes
-    #epsilon_decay_step = (args.epsilon - args.epsilon_final) / args.episodes
-
-    flag_print = False
-
     evaluating = False
     while not evaluating:
-        #print('alpha - ', alpha, ', epsilon ', epsilon)
-        # Perform a training episode
-        #alpha = args.alpha - env.episode * alpha_decay_step
-        #epsilon = args.epsilon - env.episode * epsilon_decay_step
-
         state, done = env.reset(evaluating), False
         while not done:
             if args.render_each and env.episode and env.episode % args.render_each == 0:
                 env.render()
 
-            if flag_print:
-                print('State')
-                print(state)
-
-            # TODO: Choose `action` according to epsilon-greedy strategy
+            # Choose `action` according to epsilon-greedy strategy
             if np.random.uniform() > epsilon:
                 action = np.argmax(W[state].sum(axis=0))  # epsilon-greedy
             else:
                 action = np.random.randint(env.actions)
 
-            if flag_print:
-                print('Action ', action)
-
             next_state, reward, done, _ = env.step(action)
-
-            if flag_print:
-                print('Next State')
-                print(next_state)
 
             # TODO: Update W values
             Q_s_a = W[state, action].sum()
             Q_next_s_max = np.amax(W[next_state].sum(axis=0))
-            for i in state:
-                W[i, action] += alpha * (reward + args.gamma * Q_next_s_max - Q_s_a)
-            # W[state, action] += alpha * (reward + args.gamma * Q_next_s_max - Q_s_a)
+            # for i in state:
+            #     W[i, action] += alpha * (reward + args.gamma * Q_next_s_max - Q_s_a)
+            W[state, action] += alpha * (reward + args.gamma * Q_next_s_max - Q_s_a)
 
-            state = copy.deepcopy(next_state)
+            state = next_state
             if done:
                 break
 
-        # TODO: Decide if we want to start evaluating
+        # Decide if we want to start evaluating
 
         if not evaluating:
             if args.epsilon_final:
@@ -93,7 +83,6 @@ if __name__ == "__main__":
     for _ in range(100):
         state, done = env.reset(evaluating), False
         while not done:
-            # TODO: choose action as a greedy action
+            # choose action as a greedy action
             action = np.argmax(W[state].sum(axis=0))  # epsilon-greedy
-            next_state, reward, done, _ = env.step(action)
-            state = copy.deepcopy(next_state)
+            state, reward, done, _ = env.step(action)
