9ea13af833502c238b4d60658981524fa587a914
Karel Ha <mathemage@gmail.com>
2018-11-24 23:53:55 +0100

    Add ReCodEx team comments
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- labs/05/q_network.py
+++ labs/05/q_network.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
+#
+# All team solutions **must** list **all** members of the team.
+# The members must be listed using their ReCodEx ids anywhere
+# in the first comment block in the source file, i.e., in the first
+# consecutive range of lines beginning with `#`.
+#
+# You can find out ReCodEx id on URL when watching ReCodEx profile.
+# The id has the following format: 01234567-89ab-cdef-0123-456789abcdef.
+#
+# 090fa5b6-d3cf-11e8-a4be-00505601122b (Jan Rudolf)
+# 08a323e8-21f3-11e8-9de3-00505601122b (Karel Ha)
+#
 import collections
 
 import cart_pole_evaluator
