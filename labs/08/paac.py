13836d7ff26419701081978afed43e805ca07aee
Karel Ha <mathemage@gmail.com>
2018-12-11 19:01:25 +0100

    Set threads and workers to 4
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- labs/08/paac.py
+++ labs/08/paac.py
@@ -91,8 +91,8 @@ if __name__ == "__main__":
 	parser.add_argument("--hidden_layer", default=100, type=int, help="Size of hidden layer.")
 	parser.add_argument("--learning_rate", default=0.001, type=float, help="Learning rate.")
 	parser.add_argument("--render_each", default=0, type=int, help="Render some episodes.")
-	parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
-	parser.add_argument("--workers", default=8, type=int, help="Number of parallel workers.")
+	parser.add_argument("--threads", default=4, type=int, help="Maximum number of threads to use.")
+	parser.add_argument("--workers", default=4, type=int, help="Number of parallel workers.")
 	args = parser.parse_args()
 
 	# Create the environment
